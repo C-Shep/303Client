10290e4c668bbe1dfccacce47a37243d96c199f2
C-Shep <2101007@uad.ac.uk>
2023-11-18 18:09:04 +0000

    Reworked All Code
    From Server Based to P2P

--- clientProject/sfmlProject/client.cpp
+++ clientProject/sfmlProject/client.cpp
@@ -22,42 +22,67 @@ sf::Packet& operator >> (sf::Packet& packet, Player& player)
 
 int main()
 {
-	//Player me;
-	//Player other;
-
-	int myID;
-
-	Player playerArray[2];
-
 	sf::IpAddress inputAddress;
-
-	std::cout << "Enter the Address of Server: ";
-	std::cin >> inputAddress;
-	std::cout << "\nThanks\n";
-
-	std::cout << "Enter 0 or 1: ";
-	std::cin >> myID;
-	std::cout << "\nThanks\n";
-
-
-
-	std::cout << "Start Client\n";
 	sf::TcpSocket socket;
-	sf::Socket::Status status = socket.connect(inputAddress, 53000);
-	if (status != sf::Socket::Done)
+	short port = 53000;
+	char userType = '?';
+
+	sf::Vector2f myPos, theirPos;
+
+	do
 	{
-		std::cout << "ERROR\n";
-	}
-	else {
-		std::cout << "CONNECTED\n";
-	}
+		std::cout << "Enter s for server player or c for client player - ";
+		std::cin >> userType;
+		std::cout << "Thanks...";
 
-	sf::RenderWindow window(sf::VideoMode(200, 200), "Pong!");
-	sf::CircleShape myPaddle(10.0f);
+		if (userType == 's')
+		{
+			sf::TcpListener listener;
+			listener.listen(port);
+			listener.accept(socket);
+			std::cout << "\nWelcome to Pong, Server Player\n";
+			myPos = sf::Vector2f(10.0f,200.0f);
+		}
+		else if (userType == 'c') {
+			std::cout << "\nEnter the Address of Server: ";
+			std::cin >> inputAddress;
+			std::cout << "\nThanks, Client Player\n";
+
+			sf::Socket::Status status = socket.connect(inputAddress, 53000);
+			if (status != sf::Socket::Done)
+			{
+				std::cout << "Hmmm... Couldn't connect. Please try again!\n";
+				userType = '?';
+			}
+			else {
+				std::cout << "Connected to Server Player!\n";
+				myPos = sf::Vector2f(380.0f, 200.0f);
+			}
+		}
+		else {
+			std::cout << "\nError! Invalid user type. Please try again.\n";
+		}
+	}while (userType != 's' && userType != 'c');
+
+	std::cout << "Let us...!\n";
+	
+
+
+	sf::RenderWindow window(sf::VideoMode(400, 400), "Pong!");
+
+	sf::RectangleShape myPaddle;
+	myPaddle.setSize(sf::Vector2f(10.0f, 40.0f));
 	myPaddle.setFillColor(sf::Color::Green);
+	myPaddle.setPosition(myPos);
 
-	sf::CircleShape theirPaddle(20.0f);
+	sf::RectangleShape theirPaddle;
+	theirPaddle.setSize(sf::Vector2f(10.0f, 40.0f));
 	theirPaddle.setFillColor(sf::Color::Red);
+
+
+	std::cout << "PONG!\n";
+
+	bool update = false;
 
 	while (window.isOpen())
 	{
@@ -69,57 +94,61 @@ int main()
 				window.close();
 			}
 
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+			if (event.type == sf::Event::GainedFocus)
+			{ 
+				update = true;
+			}
+
+			if (event.type == sf::Event::LostFocus)
 			{
-				playerArray[myID].yy -= 3.0f;
-
+				update = false;
 			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-			{
-				playerArray[myID].yy += 3.0f;
-			}
-
-			//Set Shapes Pos
-			myPaddle.setPosition(playerArray[myID].xx, playerArray[myID].yy);
-
-			//Packet
-			sf::Packet packet;
-			packet << playerArray[myID];
-
-			//Send my data to the server
-			if (socket.send(packet) != sf::Socket::Done)
-			{
-				std::cout << "ERROR\n";
-			}
-
-			//Receive Data
-			sf::Packet packet2;
-
-			if (socket.receive(packet2) == sf::Socket::Done)
-			{
-				if (packet2 >> playerArray[0] >> playerArray[1])
-				{
-					std::cout << "Received Position: " << (playerArray[0].yy) << " From Player(0): " << (playerArray[0].id) << "\n";
-					std::cout << "Received Position: " << (playerArray[1].yy) << " From Player(1): " << (playerArray[1].id) << "\n";
-				}
-			}
-
-			if (myID == 0)
-			{
-			//	other.yy = playerArray[1].yy;
-				theirPaddle.setPosition(playerArray[1].xx, playerArray[1].yy);
-			}
-			else {
-			//	other.yy = playerArray[0].yy;
-				theirPaddle.setPosition(playerArray[0].xx, playerArray[0].yy);
-			}
-
-
-			window.clear();
-			window.draw(myPaddle);
-			window.draw(theirPaddle);
-			window.display();
 		}
+
+		sf::Vector2f moveVec = sf::Vector2f(0.0f,0.0f);
+
+		if (update)
+		{
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && myPaddle.getPosition().y > 0)
+			{
+				moveVec = sf::Vector2f(0.0f, -0.5f);
+				myPaddle.move(moveVec);
+
+			}
+			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && myPaddle.getPosition().y < 360)
+			{
+				moveVec = sf::Vector2f(0.0f, 0.5f);
+				myPaddle.move(moveVec);
+			}
+		}
+			
+
+		//Set Shapes Pos
+		//myPaddle.setPosition(playerArray[myID].xx, playerArray[myID].yy);
+
+		//Packet
+		sf::Packet packet;
+		packet << myPaddle.getPosition().x << myPaddle.getPosition().y;
+
+		//Send my data to the server
+		if (socket.send(packet) != sf::Socket::Done)
+		{
+			std::cout << "Error, Can't sent packet to other player :(\n";
+		}
+
+		if (socket.receive(packet) == sf::Socket::Done)
+		{
+			if (packet >> theirPos.x >> theirPos.y)
+			{
+				theirPaddle.setPosition(theirPos.x, theirPos.y);
+			}
+		}
+
+		window.clear();
+		window.draw(myPaddle);
+		window.draw(theirPaddle);
+		window.display();
+		
 	}
 
 	std::cout << "End\n";
