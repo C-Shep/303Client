fc3dc6f5a5af929ad6e14d234eafa0512221e642
C-Shep <2101007@uad.ac.uk>
2023-11-19 23:38:56 +0000

    Working Game, Could Pass :)

--- clientProject/sfmlProject/client.cpp
+++ clientProject/sfmlProject/client.cpp
@@ -1,24 +1,10 @@
 #include <SFML/Network.hpp>
 #include <SFML/Network/IpAddress.hpp>
 #include <SFML/Graphics.hpp>
+#include <string>
 #include <iostream>
 
-struct Player
-{
-	int id = 0;
-	float xx = 10.0f;
-	float yy = 10.0f;
-};
-
-sf::Packet& operator << (sf::Packet& packet, const Player& player)
-{
-	return packet << player.id << player.xx << player.yy;
-}
-
-sf::Packet& operator >> (sf::Packet& packet, Player& player)
-{
-	return packet >> player.id >> player.xx >> player.yy;
-}
+#define SPEED 200
 
 int main()
 {
@@ -27,9 +13,30 @@ int main()
 	short port = 53000;
 	char userType = '?';
 
-	sf::Vector2f myPos, theirPos;
+	sf::Vector2f myPos, theirPos, puckPos;
 	int lastDir = 1;
+	int theirLastDir = 1;
 	float puckY = 0.1;
+	sf::Clock dtClock;
+	float dt = 1.0f;
+
+
+	srand((unsigned)time(NULL));
+
+	sf::Font font;
+
+	if (!font.loadFromFile("arial.ttf"))
+	{
+		//oh no :(
+	}
+
+	sf::Text scoreText;
+	scoreText.setFont(font);
+	scoreText.setCharacterSize(16);
+	scoreText.setFillColor(sf::Color::White);
+	scoreText.setPosition(200.f,32.f);
+
+	int scores[2] = {0 , -1};
 
 	do
 	{
@@ -43,7 +50,7 @@ int main()
 			listener.listen(port);
 			listener.accept(socket);
 			std::cout << "\nWelcome to Pong, Server Player\n";
-			myPos = sf::Vector2f(10.0f,200.0f);
+			myPos = sf::Vector2f(10.0f,112.5f);
 		}
 		else if (userType == 'c') {
 			std::cout << "\nEnter the Address of Server: ";
@@ -58,7 +65,7 @@ int main()
 			}
 			else {
 				std::cout << "Connected to Server Player!\n";
-				myPos = sf::Vector2f(380.0f, 200.0f);
+				myPos = sf::Vector2f(380.0f, 112.5f);
 			}
 		}
 		else {
@@ -70,7 +77,7 @@ int main()
 	
 
 
-	sf::RenderWindow window(sf::VideoMode(400, 400), "Pong!");
+	sf::RenderWindow window(sf::VideoMode(400, 225), "Pong!");
 
 	sf::RectangleShape myPaddle;
 	myPaddle.setSize(sf::Vector2f(10.0f, 40.0f));
@@ -84,18 +91,17 @@ int main()
 	sf::RectangleShape puck;
 	puck.setSize(sf::Vector2f(10.0f, 10.0f));
 	puck.setFillColor(sf::Color::Blue);
-
 	puck.setPosition(sf::Vector2f(200.f,200.f));
-
-	
-	sf::Vector2f puckDir = sf::Vector2f(-0.1,0.0);
 
 	std::cout << "PONG!\n";
 
 	bool update = false;
+	sf::Vector2f puckDir = sf::Vector2f(-1.0, 0.0);
 
+	//Game Loop
 	while (window.isOpen())
 	{
+		//SFML Events
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
@@ -104,101 +110,79 @@ int main()
 				window.close();
 			}
 
-			if (event.type == sf::Event::GainedFocus)
-			{ 
-				update = true;
-			}
+	//		if (event.type == sf::Event::GainedFocus)
+		//	{ 
+		//		update = true;
+		//	}
 
-			if (event.type == sf::Event::LostFocus)
-			{
-				update = false;
-			}
+		//	if (event.type == sf::Event::LostFocus)
+		//	{
+		//		update = false;
+		//	}
 		}
 
-		sf::Vector2f moveVec = sf::Vector2f(0.0f,0.0f);
+		dt = dtClock.restart().asSeconds();
 
-		if (update)
-		{
+		//if (update)
+		//{
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && myPaddle.getPosition().y > 0)
 			{
-				moveVec = sf::Vector2f(0.0f, -0.5f);
 				lastDir = -1;
-				myPaddle.move(moveVec);
+				myPaddle.move(sf::Vector2f(0.0f, -500.0f * dt));
 
 			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && myPaddle.getPosition().y < 360)
+			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && myPaddle.getPosition().y < 180)
 			{
-				moveVec = sf::Vector2f(0.0f, 0.5f);
 				lastDir = 1;
-				myPaddle.move(moveVec);
+				myPaddle.move(sf::Vector2f(0.0f, 500.0f * dt));
 			}
-		}
-
-		puckY = (0.1f * lastDir);
+	//	}
 
 		//The worst code youve seen in your life
-		std::cout << puck.getPosition().x << " - PuckY =" << puckY << "\n";
-
-		if (userType == 'c' && puck.getPosition().x >= 370.0f && puck.getPosition().y >= myPaddle.getPosition().y && puck.getPosition().y <= (myPaddle.getPosition().y + 40))
+		if (userType == 's')
 		{
-			puckDir = sf::Vector2f(-0.1f, puckY);
-		}
-		else if (userType == 's' && puck.getPosition().x <= 20.0f && puck.getPosition().y >= myPaddle.getPosition().y && puck.getPosition().y <= (myPaddle.getPosition().y + 40))
-		{
-			puckDir = sf::Vector2f(0.1, puckY);
-		}
-		else if (userType == 's' && puck.getPosition().x >= 370.0f && puck.getPosition().y >= theirPaddle.getPosition().y && puck.getPosition().y <= (theirPaddle.getPosition().y + 40))
-		{
-			puckDir = sf::Vector2f(-0.1f, puckY);
-		}
-		else if (userType == 'c' && puck.getPosition().x <= 20.0f && puck.getPosition().y >= theirPaddle.getPosition().y && puck.getPosition().y <= (theirPaddle.getPosition().y + 40))
-		{
-			puckDir = sf::Vector2f(0.1f, puckY);
-		}
-		else if (puck.getPosition().y > 390.f)
-		{
-			if (puckDir.x == 0.1)
+			if ( puck.getPosition().x <= 20.0f && puck.getPosition().y >= myPaddle.getPosition().y - 10.0f && puck.getPosition().y <= (myPaddle.getPosition().y + 40))
 			{
-				puckDir = sf::Vector2f(-0.1f, -0.1);
-			}
-			else {
-				puckDir = sf::Vector2f(0.1f, -0.1);
-			}
-		}
-		else if (puck.getPosition().y < 10.f)
-		{
-			if (puckDir.x == 0.1)
+				puckY = (1.f * lastDir) + rand()%3 + 0.1;
+				puckDir = sf::Vector2f(1.0f, puckY);
+			}else if (puck.getPosition().x >= 370.0f && puck.getPosition().y >= theirPaddle.getPosition().y - 10.0f && puck.getPosition().y <= (theirPaddle.getPosition().y + 40))
 			{
-				puckDir = sf::Vector2f(0.1f, 0.1);
-			}
-			else {
-				puckDir = sf::Vector2f(-0.1f, 0.1);
+				puckY = (1.f * theirLastDir) + rand()%3 + 0.1;
+				puckDir = sf::Vector2f(-1.f, puckY);
 			}
 
+			if (puck.getPosition().y > 215.f || puck.getPosition().y < 0.f)
+			{
+				puckDir.y *= -1;
+				if (puck.getPosition().y < 0.f)
+				{
+					puck.setPosition(puck.getPosition().x, 1.f);
+				}
+				else {
+					puck.setPosition(puck.getPosition().x, 214.f);
+				}
+				
+			}
+
+			puck.setPosition(puck.getPosition().x + (puckDir.x*SPEED)*dt, puck.getPosition().y + (puckDir.y * SPEED)* dt);
+			
+			if (puck.getPosition().x < 0.0f || puck.getPosition().x > 400.0f)
+			{
+				if (puck.getPosition().x < 0.0f)
+				{
+					scores[1] += 1;
+				}
+				else 
+				{
+					scores[0] += 1;
+				}
+				puck.setPosition(200.0f, 112.5f);
+			}
 		}
-
-
-
-		puck.setPosition(puck.getPosition().x + puckDir.x, puck.getPosition().y + puckDir.y);
-
-		if (puck.getPosition().x < 0.0f || puck.getPosition().x > 400.0f)
-		{
-			puck.setPosition(200.0f,200.0f);
-		}
-
-
-
-	//	if (puck.getPosition().x < (myPaddle.getPosition().x+10) && puck.getPosition().y > myPaddle.getPosition().y && puck.getPosition().y < (myPaddle.getPosition().y + 40))
-	//	{
-	//		puckDir = -puckDir;
-		//}
-
-		//Set Shapes Pos
-		//myPaddle.setPosition(playerArray[myID].xx, playerArray[myID].yy);
 
 		//Packet
 		sf::Packet packet;
-		packet << myPaddle.getPosition().x << myPaddle.getPosition().y;
+		packet << myPaddle.getPosition().x << myPaddle.getPosition().y << puck.getPosition().x << puck.getPosition().y << lastDir << scores[0] << scores[1];
 
 		//Send my data to the server
 		if (socket.send(packet) != sf::Socket::Done)
@@ -208,16 +192,30 @@ int main()
 
 		if (socket.receive(packet) == sf::Socket::Done)
 		{
-			if (packet >> theirPos.x >> theirPos.y)
+			int tempScores[2];
+			if (packet >> theirPos.x >> theirPos.y >> puckPos.x >> puckPos.y >> theirLastDir >> tempScores[0] >> tempScores[1])
 			{
 				theirPaddle.setPosition(theirPos.x, theirPos.y);
+				if (userType == 'c')
+				{
+					puck.setPosition(puckPos.x, puckPos.y);
+					scores[0] = tempScores[0];
+					scores[1] = tempScores[1];
+				}
 			}
 		}
+
+		std::string s = std::to_string(scores[0]);
+		std::string ss = std::to_string(scores[1]);
+
+
+		scoreText.setString(s + "/" + ss);
 
 		window.clear();
 		window.draw(myPaddle);
 		window.draw(theirPaddle);
 		window.draw(puck);
+		window.draw(scoreText);
 		window.display();
 		
 	}
